313b59a66bf0a92e2117d026be0a783dbd079954
krissy <krissydgroom@gmail.com>
2022-01-25 07:49:50 -0800

    Added queries css file and added functions to js

--- js/main.js
+++ js/main.js
@@ -5,41 +5,52 @@ const projects = document.querySelector(".projects-col");
 // Access body to stop scrolling behavior
 const body = document.querySelector("body");
 
+
+// Open Description
+const openDescription = (element, desc, closeBtn, projBtn, projDesc) => {
+    element.classList.add("description");
+    body.classList.add("no-scroll"); 
+    window.scrollTo(0, 0);
+
+    projBtn.classList.add("hidden");
+    desc.classList.add("hidden");
+    closeBtn.classList.remove("hidden");
+    projDesc.classList.remove("hidden");
+}
+
+
+// Close Description
+const closeDescription = (element, desc, closeBtn, projBtn, projDesc) => {
+    element.classList.remove("description");
+
+    closeBtn.classList.add("hidden");
+    projBtn.classList.remove("hidden");
+    desc.classList.remove("hidden");
+    projDesc.classList.add("hidden");
+    body.classList.add("no-scroll");
+    element.scrollIntoView({block: "center"});
+}
+
+
 // ***** Events *****
 
 // Click event on description in project  - add try catch block
 projects.addEventListener('click', function(e) {
     const desc = e.target;
     const currentNode = desc.parentNode;
-    if (desc.matches("h2")) {
-        currentNode.classList.add("description");
-        body.classList.add("no-scroll"); 
-        window.scrollTo(0, 0);
 
-        // add 'X' button
+    if (desc.matches("h2")) {
+        // get elements to toggle
         const closeButton = currentNode.querySelector(".btn-close");
         const projectImage = currentNode.querySelector(".project-img");
-
-        // hide image and description link
-        projectImage.classList.add("hidden");
-        desc.classList.add("hidden");
-        closeButton.classList.remove("hidden");
-
-        // open project description section
         const projectDesc = currentNode.querySelector(".project-description");
-        projectDesc.classList.remove("hidden");
+
+        // If user clicks on "description ->", open description section
+        openDescription(currentNode, desc, closeButton, projectImage, projectDesc);
 
         // Close description section if x is clicked
         closeButton.addEventListener("click", function(e) {
-            // 1st close description
-            currentNode.classList.remove("description");
-
-            closeButton.classList.add("hidden");
-            projectImage.classList.remove("hidden");
-            desc.classList.remove("hidden");
-            projectDesc.classList.add("hidden");
-            body.classList.add("no-scroll");
-            currentNode.scrollIntoView({block: "center"});
+            closeDescription(currentNode, desc, closeButton, projectImage, projectDesc);
         })
     } else if (body.classList.contains("no-scroll")) {
         body.classList.remove("no-scroll");
@@ -48,6 +59,39 @@ projects.addEventListener('click', function(e) {
 
 
 
+// // toggle Description - not working on close - still working on this
+// const toggleDescription = (openClose, element, desc, closeBtn, projBtn, projDesc) => {
+//     if (openClose === "open") {
+//         window.scrollTo(0, 0);
+//     } else if (openClose === "close") {
+//         element.scrollIntoView({block: "center"});
+//     }
+//     // no idea why this code is breaking when closing
+//     // description multiple times??
+//     toggleClass(element, "description");
+    
+//     // X button
+//     toggleClass(closeBtn, "hidden");
+
+//     // phone image & description header
+//     toggleClass(projBtn, "hidden");
+//     toggleClass(desc, "hidden");
+
+//     // description div
+//     toggleClass(projDesc, "hidden");
+
+//     toggleClass(body, "no-scroll"); 
+// }
+
+
+// // Toggle classes
+// const toggleClass = (element, className) => {
+//     if (element.classList.contains(className)) {
+//         element.classList.remove(className);
+//     } else {
+//         element.classList.add(className);
+//     }
+// }
 
 
 
